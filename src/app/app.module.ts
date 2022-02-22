38b965f7ac42a70eacc7a2ec7695d1178955ef2b
General Baguettson <mathieu@bigare.eu>
2022-02-22 11:42:52 +0100

    [INIT] created parallax component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ParallaxComponent } from './parallax/parallax.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ParallaxComponent
   ],
   imports: [
     BrowserModule,
